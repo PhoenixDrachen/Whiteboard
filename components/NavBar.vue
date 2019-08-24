5b4714a72941a3fff81892be15c9f3c76e55cd0a
Tyler 'PhoenixDrachen' Behunin <phoenixdrachen22@gmail.com>
2019-08-24 13:20:38 -0600

    Added Styling to NavBar Component

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,27 +1,36 @@
 <template>
     <div id="NavBar">
         <div class="navbar_wrapper">
+            <div class="icon_container">
+                <img src="/Closed-Dark.svg">
+                <span v-show="active=='Closed'">Closed</span>
+            </div>
             <div class="icon_container active">
-                <img src="/Closed-Light.svg" alt="">
+                <img src="/Numbers-Light.svg">
+                <span v-show="active=='Numbers'">Numbers</span>
             </div>
             <div class="icon_container">
-                <img src="/Numbers-Dark.svg" alt="">
+                <img src="/Taks-Dark.svg">
+                <span v-show="active=='Tasks'">Tasks</span>
             </div>
             <div class="icon_container">
-                <img src="/Taks-Dark.svg" alt="">
+                <img src="/Deals-Dark.svg">
+                <span v-show="active=='Deals'">Deals</span>
             </div>
             <div class="icon_container">
-                <img src="/Deals-Dark.svg" alt="">
-            </div>
-            <div class="icon_container">
-                <img src="/Launch-Dark.svg" alt="">
+                <img src="/Launch-Dark.svg">
+                <span v-show="active=='Launch'">Launch</span>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return { active: 'Numbers' }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
@@ -36,6 +45,11 @@ export default {}
     .icon_container{
         height:100px;
         padding:10px;
+        overflow:hidden;
+        display:flex;
+        flex-direction:row;
+        align-items:center;
+        justify-content: space-between;
         img{
             width:75px;
         }
@@ -43,6 +57,11 @@ export default {}
             background-color:$dark_base;
             flex-grow: 2;
             border-radius:25px;
+            color:$light_base;
+            font-size: 40px;
+            font-family: 'Roboto';
+            padding-right:15px;
+            font-weight:700;
         }
     }
 }

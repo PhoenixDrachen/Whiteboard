91cf63492a42c517bc48e54787ef714c71229d5e
Tyler 'PhoenixDrachen' Behunin <phoenixdrachen22@gmail.com>
2019-08-27 02:11:52 -0600

    Updating NavBar component functionality

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,31 +1,13 @@
 <template>
     <div id="NavBar">
         <div class="navbar_wrapper">
-            <div :class="{'icon_container':true, 'active':(active == 'Closed')}">
-                <img v-if="active == 'Closed'" src="/Closed-Light.svg">
-                <img v-else src="/Closed-Dark.svg" @click="active='Closed'">
-                <span v-if="active=='Closed'">Closed</span>
-            </div>
-            <div :class="{'icon_container':true, 'active':(active == 'Numbers')}">
-                <img v-if="active == 'Numbers'" src="/Numbers-Light.svg">
-                <img v-else src="/Numbers-Dark.svg" @click="active='Numbers'">
-                <span v-if="active=='Numbers'">Numbers</span>
-            </div>
-            <div :class="{'icon_container':true, 'active':(active == 'Tasks')}">
-                <img v-if="active == 'Tasks'" src="/Tasks-Light.svg">
-                <img v-else src="/Taks-Dark.svg" @click="active='Tasks'">
-                <span v-if="active=='Tasks'">Tasks</span>
-            </div>
-            <div :class="{'icon_container':true, 'active':(active == 'Deals')}">
-                <img v-if="active == 'Deals'" src="/Deals-Light.svg">
-                <img v-else src="/Deals-Dark.svg" @click="active='Deals'">
-                <span v-if="active=='Deals'">Deals</span>
-            </div>
-            <div :class="{'icon_container':true, 'active':(active == 'Launch')}">
-                <img v-if="active == 'Launch'" src="/Launch-Light.svg">
-                <img v-else src="/Launch-Dark.svg" @click="active='Launch'">
-                <span v-if="active=='Launch'">Launch</span>
-            </div>
+            <transition-group tag="div">
+                <div v-for="icon in icons" :key="icon" :class="{'icon_container':true, 'active':(active == icon)}">
+                    <img v-if="active == icon" :src="'/'+ icon + '-Light.svg'">
+                    <img v-else :src="'/'+ icon + '-Dark.svg'" @click="active = icon">
+                    <span>{{ icon }}</span>
+                </div>
+            </transition-group>
         </div>
     </div>
 </template>
@@ -33,7 +15,10 @@
 <script>
 export default {
   data () {
-    return { active: 'Launch' }
+    return {
+      active: 'Launch',
+      icons: ['Closed', 'Totals', 'Tasks', 'Deals', 'Launch']
+    }
   }
 }
 </script>
@@ -41,7 +26,7 @@ export default {
 <style lang="scss" scoped>
 @import '~/assets/variables';
 
-.navbar_wrapper{
+.navbar_wrapper>div{
     display: flex;
     flex-direction: row;
     justify-content: space-between;
@@ -49,27 +34,33 @@ export default {
     height:125px;
     .icon_container{
         height:100px;
+        width:85px;
         padding:10px;
         overflow:hidden;
         display:flex;
         flex-direction:row;
         align-items:center;
         justify-content: space-between;
-        flex-grow: 1;
         border-radius:25px;
-        transition: flex-grow 2s ease, background-color 2s ease;
+        transition:width 1s ease, background-color .5s ease;
         img{
             width:75px;
+            margin-right:10px
+        }
+        span{
+            visibility: hidden;
         }
         &.active{
             background-color:$dark_base;
-            flex-grow: 2;
+            width:250px;
             color:$light_base;
             font-size: 40px;
             font-family: 'Roboto';
             padding-right:15px;
             font-weight:700;
-            transition:flex-grow 2s ease, background-color 1s ease
+            // span{
+            //     visibility: visible;
+            // }
         }
     }
 }

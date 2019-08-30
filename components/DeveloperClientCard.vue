78f679c45429115ceec51910591a17bf8c3bbffb
Tyler 'PhoenixDrachen' Behunin <phoenixdrachen22@gmail.com>
2019-08-30 02:02:51 -0600

    working on client UI and Functionality

--- components/DeveloperClientCard.vue
+++ components/DeveloperClientCard.vue
@@ -1,12 +1,40 @@
 <template>
-    <div>
-        
+    <div class="developerClientCard">
+        <h2>{{ this.developerName }}</h2>
     </div>
 </template>
 
 <script>
-    export default {}
+export default {
+  props: {
+    developer: Object
+  },
+  data () {
+    return {
+      developerName: this.developer.name
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
+@import '~/assets/variables.scss';
+    .developerClientCard{
+        width:100%;
+        height:550px;
+        background-color: $dark_accent;
+        border-radius:25px;
+        box-shadow: 10px 10px 20px rgba(0,0,0,.5);
+        padding-left:5px;
+        padding-right:5px;
+        h2{
+            text-align: center;
+            padding-top:15px;
+            font-size:45px;
+            color:$light_base;
+            font-family: 'Cuprum';
+            font-weight:700;
+            border-bottom: 8px solid $light_base;
+        }
+    }
 </style>
--- pages/clients.vue
+++ pages/clients.vue
@@ -1,12 +1,41 @@
 <template>
-    <div>
-        clients
+    <div id="clients" >
+        <div class="cardContainer" v-for="developer in developers" :key="developer.name" >
+            <DeveloperClientCard :developer="developer" />
+        </div>
     </div>
 </template>
 
 <script>
-export default {}
+import DeveloperClientCard from '../components/DeveloperClientCard'
+export default {
+  components: { DeveloperClientCard },
+  data () {
+    return {
+      developers: [
+        {
+          name: 'Josh'
+        },
+        {
+          name: 'Jeff'
+        },
+        {
+          name: 'Seth'
+        }
+      ]
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
+    #clients{
+        display: flex;
+        flex-direction: row;
+        margin:20px;
+        justify-content:space-between;
+        .cardContainer{
+            width:calc(33% - 10px);
+        }
+    }
 </style>
